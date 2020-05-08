f5083583b1e02b353d9b26639a780098ca9340f3
kkm <kubasurdej@gmail.com>
2020-05-08 17:20:00 +0200

    Modify config and config checks

--- src/cfgCheck.ts
+++ src/cfgCheck.ts
@@ -1,13 +1,29 @@
-import config from "./config";
 import chalk from "chalk";
+import "./config";
 
-if (!config.token) {
+if (!process.env.DISCORD_TOKEN) {
   console.log(chalk.red("DISCORD_TOKEN not specified. Exiting program."));
   process.exit(1);
 }
 
-if (!config.password) {
+if (!process.env.PREFIX) {
+  console.log(chalk.blue("PREFIX not specified. Using '?' instead."));
+} else {
+  console.log(chalk.blue(`Prefix set to '${process.env.PREFIX}'`));
+}
+
+if (!process.env.LAVALINK_PASSWORD) {
   console.log(
     chalk.yellow("LAVALINK_PASSWORD not specified. Using empty string instead.")
   );
+}
+
+if (!process.env.LAVALINK_HOST) {
+  console.log(
+    chalk.yellow("LAVALINK_HOST not specified. Using localhost instead.")
+  );
+}
+
+if (!process.env.LAVALINK_PORT) {
+  console.log(chalk.yellow("LAVALINK_PORT not specified. Using 2333 instead."));
 }
--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,9 @@ import "dotenv/config";
 export default {
   prefix: process.env.PREFIX || "?",
   token: process.env.DISCORD_TOKEN,
-  password: process.env.LAVALINK_PASSWORD || "",
+  lavalink: {
+    host: process.env.LAVALINK_HOST || "localhost",
+    password: process.env.LAVALINK_PASSWORD || "",
+    port: process.env.LAVALINK_PORT || "2333",
+  },
 };
