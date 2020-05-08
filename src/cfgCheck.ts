44649e7d055cb7fea267268cf2b8f3c7ae6a2a63
kkm <kubasurdej@gmail.com>
2020-05-08 15:40:50 +0200

    Fix chalk import

--- src/cfgCheck.ts
+++ src/cfgCheck.ts
@@ -1,5 +1,5 @@
 import config from "./config";
-import * as chalk from "chalk";
+import chalk from "chalk";
 
 if (!config.token) {
   console.log(chalk.red("DISCORD_TOKEN not specified. Exiting program."));
