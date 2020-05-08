79f879ac8c68644fb2db082bb3c49a5c17b0927a
kkm <kubasurdej@gmail.com>
2020-05-08 16:27:02 +0200

    Add lavalink password to config

--- src/cfgCheck.ts
+++ src/cfgCheck.ts
@@ -5,3 +5,9 @@ if (!config.token) {
   console.log(chalk.red("DISCORD_TOKEN not specified. Exiting program."));
   process.exit(1);
 }
+
+if (!config.password) {
+  console.log(
+    chalk.yellow("LAVALINK_PASSWORD not specified. Using empty string instead.")
+  );
+}
--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ import "dotenv/config";
 export default {
   prefix: process.env.PREFIX || "?",
   token: process.env.DISCORD_TOKEN,
+  password: process.env.LAVALINK_PASSWORD || "",
 };
