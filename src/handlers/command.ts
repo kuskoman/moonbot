f754111b005e5f7572255c507de5289ba11f95a9
kkm <kubasurdej@gmail.com>
2020-05-09 17:31:54 +0200

    Create simple commandHandler

---
+++ src/handlers/command.ts
@@ -0,0 +1,34 @@
+import { Msg } from "../types/general.interfaces";
+import { Command } from "../types/command.interfaces";
+import config from "../config";
+
+const commands: Map<string, Command> = new Map();
+
+export const loadCommand = (command: Command): void => {
+  command.patterns.forEach((pattern) => {
+    commands.set(pattern, command);
+  });
+};
+
+export const handleCommand = async (msg: Msg): Promise<void> => {
+  const content = msg.content;
+  if (!(content && content.startsWith(config.prefix))) return;
+
+  const commandPattern = extractCommand(msg);
+  const _commandArgs = extractArgs(msg);
+
+  if (!commandPattern) return;
+
+  const command = commands.get(commandPattern);
+  if (!command) return;
+
+  command.execute(msg);
+};
+
+const extractCommand = (msg: Msg) => {
+  return msg.content?.slice(config.prefix.length).split(" ")[0];
+};
+
+const extractArgs = (msg: Msg) => {
+  return msg.content?.slice(config.prefix.length).split(" ").slice(1).join(" ");
+};
